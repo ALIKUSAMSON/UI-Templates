4704ff27af7e19cb8d0774dc600c6a0dfd884bc3
ALIKUSAMSON <alikusamson2014@gmail.com>
2017-08-15 17:09:33 -0700

    Templates

---
+++ Dict/shopping_list.py
@@ -0,0 +1,13 @@
+myshopping_list ={"shoes":3000,"T-shirts":1000,"Tie":500}
+#Read
+print myshopping_list
+
+#Update
+myshopping_list.update({"belt":2500})
+print myshopping_list
+
+#Delete
+del myshopping_list['shoe']
+print myshopping_list
+
+#Create
